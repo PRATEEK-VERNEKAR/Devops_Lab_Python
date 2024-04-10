b5ab4662111a63bee35f50e8b4e29e2ea52a4386
Prateek M Vernekar <107637873+PRATEEK-VERNEKAR@users.noreply.github.com>
2024-04-10 11:47:21 +0530

    Update test_module.py

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.mymodule import add  # Assuming 'mymodule.py' is located in 'src' directory
+from src.mymodule import add  
 
 import unittest
 
